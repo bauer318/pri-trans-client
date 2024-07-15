1015bad4fb65f881c1e266dc5788e3277d360a4b
jacques <jacqueskibamba@gmail.com>
2024-07-16 01:40:35 +0300

    git commit changed ip

--- src/services/Utils.jsx
+++ src/services/Utils.jsx
@@ -222,7 +222,7 @@ export const getAgentAccountRq = (account, connectedUser) => {
         country: connectedUser?.country?.countryName
     }
 }
-export const baseURL = 'http://193.187.174.234:8080/api';
+export const baseURL = 'http://192.144.12.63:8080/api';
 //export const baseURL = 'http://localhost:8080/api';
 const instance = axios.create({
     baseURL: baseURL,
