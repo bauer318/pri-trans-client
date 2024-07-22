d0ef12e6f5498f471ba6ee22af0e644a5fa9c103
jacques <jacqueskibamba@gmail.com>
2024-07-23 01:57:47 +0300

    updated dependencies

--- src/services/Utils.jsx
+++ src/services/Utils.jsx
@@ -222,8 +222,8 @@ export const getAgentAccountRq = (account, connectedUser) => {
         country: connectedUser?.country?.countryName
     }
 }
-export const baseURL = 'http://192.144.12.63:8080/api';
-//export const baseURL = 'http://localhost:8080/api';
+//export const baseURL = 'http://192.144.12.63:8080/api';
+export const baseURL = 'http://localhost:8080/api';
 const instance = axios.create({
     baseURL: baseURL,
     headers: {Authorization: getItem('jwtToken')}
